459e387710daa35ccb698251a311a6f6bbd71f50
Kaca <kaca1707@gmail.com>
2019-03-17 14:08:34 +0100

    feat(home): implement API

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import './registerServiceWorker'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueFilterDateFormat from 'vue-filter-date-format'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueFilterDateFormat)
 
 new Vue({
   router,
